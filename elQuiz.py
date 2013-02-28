085e32fba09433412b1603054b67fa33388ffb5d
JORGE ANDRES MADRID WOLFF <joanmawo@hotmail.com>
2013-02-28 14:23:01 -0500

    Quiz febrero 28 de 2013

---
+++ elQuiz.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+frecuencias = [20]
+rango = arange(20)+1
+
+filein = codecs.open("frecuencias"+sys.argv[1], "r", "utf8")
+        
+frecuencias = np.loadtxt(filein)        
+
+m = np.polyfit(log(rango), log(frecuencias), 1)
+        
+k = m[0]
+
+fileout = codecs.open("coeficientesDe"+sys.argv[1], "w", "utf8")
+fileout.write("%s %f\n", sys.argv[1], k)
+
+filein.close()
